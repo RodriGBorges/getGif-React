2ad8213be518a34c4ab566712d08b685c0ba6b50
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-01 21:14:01 -0300

    Arreglo error de url

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -3,7 +3,7 @@ export const getGifs = async (category) => {
     try {
         const apiKey = "LW4ClBerndcomn8ovkeRGA0K16oNAs3Y";
 
-        const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&q=${category}&limit=10&rating=g`
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`
 
         const response = await fetch(url);
 
