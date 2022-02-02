e3a2f41ce0ccc007289f5d8e92870e253bb7f89c
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-01 21:16:44 -0300

    Componente agregar categoria

--- src/components/addCategory.js
+++ src/components/addCategory.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-export const addCategory = ({setCategories}) => {
+export const AddCategory = ({setCategories}) => {
 
     const [inputValue, setInputValue] = useState('');
 
